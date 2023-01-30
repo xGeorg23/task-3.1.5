ab1a0165d1887e12896d0e2b446971b13c2d26a0
master <gera2001.11.04@gmail.com>
2023-01-30 13:34:22 +0300

    Initial commit

---
+++ src/main/java/com/example/task315/Application.java
@@ -0,0 +1,26 @@
+package com.example.task315;
+
+import com.example.task315.config.CookieConfig;
+import com.example.task315.model.User;
+import com.example.task315.service.WebService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CookieConfig.class);
+        WebService WebService = context.getBean("webService", WebService.class);
+        WebService.getAllUsers();
+        WebService.saveUser(new User(3L, "", "Brown", (byte) 40));
+        WebService.updateUser(new User(3L, "Thomas", "Shelby", (byte) 30));
+        WebService.deleteUser(3L);
+
+        System.out.println(WebService.result);
+
+    }
+
+}
